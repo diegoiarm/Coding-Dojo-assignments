35b36ebbcb4f51dbc6ceb8c2ca40c8ba22eb9670
Diego ⠀ <iarmdiego@outlook.cl>
2023-08-07 19:41:57 -0400

    Upload juegoNinjaGold project

---
+++ Java Spring/Spring Fundamentals/juegoNinjaGold/src/test/java/com/codingdojo/juegoninjagold/JuegoNinjaGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.juegoninjagold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoNinjaGoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
