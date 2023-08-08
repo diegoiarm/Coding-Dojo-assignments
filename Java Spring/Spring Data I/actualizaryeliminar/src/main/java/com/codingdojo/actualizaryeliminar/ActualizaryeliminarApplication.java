8410ea66430cdd7799b0a57d4ad180279ed6ca38
Diego ⠀ <iarmdiego@outlook.cl>
2023-08-08 14:33:50 -0400

    Upload actualizaryeliminar project

---
+++ Java Spring/Spring Data I/actualizaryeliminar/src/main/java/com/codingdojo/actualizaryeliminar/ActualizaryeliminarApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.actualizaryeliminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualizaryeliminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualizaryeliminarApplication.class, args);
+	}
+
+}
---
+++ Java Spring/Spring Data I/actualizaryeliminar/src/test/java/com/codingdojo/actualizaryeliminar/ActualizaryeliminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.actualizaryeliminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActualizaryeliminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
